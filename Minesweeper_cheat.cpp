d82ab20eac40edc1acb0f84d3d63384b3808fabd
manicslope <damir.nurutdinov97@gmail.com>
2020-07-26 01:56:20 +0300

    First try on manipulate memory with c++
    Code founds bombs in memory dump and put flags instead of the bomb which results in 1 second record

---
+++ Minesweeper_cheat.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+	HWND hwnd = FindWindowA(NULL, "Minesweeper");
+	//cout << "hi";
+	if (!hwnd)
+		cout << "no hwnd" << endl;
+	DWORD pid;
+	GetWindowThreadProcessId(hwnd, &pid);
+	if (!pid)
+		cout << "no pid" << endl;
+	HANDLE h = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+	//int error = GetLastError();
+	//cout << error << endl;
+	if (!h)
+		cout << "no h" << endl;
+	int value;
+	int i = 0;
+	int n = 0x0e;
+	// DWORD address = 0x01005340;
+	for (int address = 0x01005350; address <= 0x01005480; address++) {
+		ReadProcessMemory(h, (LPVOID)address, &value, 1, 0);
+		value = value & 0xff;
+		if (value == 0x8f)
+			WriteProcessMemory(h, (PBYTE*)address, &n, 1, 0);
+		if ((i % 2 != 0) && (value != 0x10))
+			cout << std::hex << value << " ";
+		if (value == 0x10) {
+			cout << endl;
+			i++;
+		}
+	}
+	cin.get();
+}
